1c39489051dd98071fc21f8ea7c252a3179dbfbc
olav35 <fosseolav@gmail.com>
2020-03-11 14:12:46 +0100

    Finish exercise 1.1

---
+++ part1/src/index.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = (props) => <h1>{props.course}</h1>
+
+const Content = (props) => {
+  return props.parts.map(part => (
+    <p>{part.name} {part.exercises}</p>
+  )
+  )
+}
+
+const Total = (props) => {
+  const total = props.parts.reduce((sum, part) => sum + part.exercises, props.parts[0].exercises)
+  return (
+    <p>
+      Number of exercises {total}
+    </p>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+  const parts = [{name: part1, exercises: exercises1},
+                 {name: part2, exercises: exercises2},
+                 {name: part3, exercises: exercises3}]
+
+  return (
+    <div>
+      <Header course={course}/>
+      <Content parts={parts}/>
+      <Total parts={parts} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
