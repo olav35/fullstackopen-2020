2afa7a8f462728a145ec363a4ba860b515014607
olav35 <fosseolav@gmail.com>
2020-03-12 11:51:09 +0100

    Solve exercise 2.2

--- part1/course-information/src/App/Course/Total.js
+++ part1/course-information/src/App/Course/Total.js
@@ -2,11 +2,7 @@ import React from 'react'
 
 const Total = (props) => {
   const total = props.course.parts.reduce((sum, part) => sum + part.exercises, props.course.parts[0].exercises)
-  return (
-    <p>
-      Number of exercises {total}
-    </p>
-  )
+  return <b>total of {total} exercises </b>
 }
 
 export default Total
