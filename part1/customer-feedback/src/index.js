dd214d88b06267a2daaa7ddd6575a0c35ca3092a
olav35 <fosseolav@gmail.com>
2020-03-12 08:56:20 +0100

    Solve 1.7 and 1.8

--- part1/customer-feedback/src/index.js
+++ part1/customer-feedback/src/index.js
@@ -11,12 +11,18 @@ const Feedback = (props) => (
 )
 
 const Statistics = ({good, neutral, bad}) => {
+  const all = good + neutral + bad
+  const average = (good - bad) / all || 0
+  const positive = (good / all) * 100 || 0
   return (
     <div>
       <h1>statistics</h1>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive}%</p>
     </div>
   )
 }
