57ac6e6fb0c900d35c67a6916e98e25680e6d4d2
olav35 <fosseolav@gmail.com>
2020-03-12 09:04:24 +0100

    Solve exercise 1.9

--- part1/customer-feedback/src/index.js
+++ part1/customer-feedback/src/index.js
@@ -14,17 +14,22 @@ const Statistics = ({good, neutral, bad}) => {
   const all = good + neutral + bad
   const average = (good - bad) / all || 0
   const positive = (good / all) * 100 || 0
-  return (
-    <div>
-      <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {all}</p>
-      <p>average {average}</p>
-      <p>positive {positive}%</p>
-    </div>
-  )
+  if ([good, neutral, bad].every(score => score === 0)){
+    return <p>No feedback given</p>
+  }
+  else {
+    return (
+      <div>
+        <h1>statistics</h1>
+        <p>good {good}</p>
+        <p>neutral {neutral}</p>
+        <p>bad {bad}</p>
+        <p>all {all}</p>
+        <p>average {average}</p>
+        <p>positive {positive}%</p>
+      </div>
+    )
+  }
 }
 
 const App = () => {
