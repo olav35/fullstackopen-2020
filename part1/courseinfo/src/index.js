b1b85910e1ce2f23286b80e5207a4621bb52d722
olav35 <fosseolav@gmail.com>
2020-03-20 12:30:15 +0100

    Downgrade part1 courseinfo and add part2 courseinfo

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,5 +1,52 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App/App'
+
+const Header = (props) => <h1>{props.course.name}</h1>
+
+const Part = (props) => <p>{props.name} {props.exercises}</p>
+
+const Content = (props) => {
+  return props.course.parts.map(part => (
+    <Part name={part.name} exercises={part.exercises}/>
+  )
+  )
+}
+
+const Total = (props) => {
+  const total = props.course.parts.reduce((sum, part) => sum + part.exercises, props.course.parts[0].exercises)
+  return (
+    <p>
+      Number of exercises {total}
+    </p>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Header course={course}/>
+      <Content course={course}/>
+      <Total course={course} />
+    </div>
+  )
+}
 
 ReactDOM.render(<App />, document.getElementById('root'))
