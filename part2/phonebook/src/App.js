c562294292108ae5d9d1534b5903f64573834aae
olav35 <fosseolav@gmail.com>
2020-03-28 19:02:40 +0100

    Solve exercise 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,12 +1,19 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
+import axios from "axios"
 import PersonForm from "./components/PersonForm"
 import Filter from "./components/Filter"
 import Persons from "./components/Persons"
 
 const App = () => {
-  const [ persons, setPersons] = useState([
-    { name: "Arto Hellas" , number: "12345678"}
-  ])
+  const [ persons, setPersons] = useState([])
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/persons")
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
 
   const [ newName, setNewName ] = useState("")
   const [ newNumber, setNewNumber ] = useState("")
