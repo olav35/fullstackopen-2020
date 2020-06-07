64bf6a5d7c5b8a7e75c9467088d75ba25a330ad3
Olav Fosse <fosseolav@gmail.com>
2020-06-07 21:07:27 +0200

    Finish exercise 1.2

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -5,16 +5,20 @@ const Header = (props) => (
   <h1>{props.course}</h1>
 )
 
+const Part = (props) => (
+  <p>{props.part} {props.exercises}</p>
+)
+
 const Content = (props) => (
   <div>
-    <p>{props.part1} {props.exercises1}</p>
-    <p>{props.part2} {props.exercises2}</p>
-    <p>{props.part3} {props.exercises3}</p>
+    <Part part={props.part1} exercises={props.exercises1} />
+    <Part part={props.part2} exercises={props.exercises2} />
+    <Part part={props.part3} exercises={props.exercises3} />
   </div>
 )
 
 const Total = (props) => (
-  <p>Number of exercises {props.total}</p>
+  <p>Number of exercises {props.count}</p>
 )
 
 const App = () => {
@@ -30,7 +34,7 @@ const App = () => {
   return (
     <div>
       <Header course={course}/>
-      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3}/>
+      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3}/>
       <Total count={exercises1 + exercises2 + exercises3}/>
     </div>
   )
