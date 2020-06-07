7b2a0cad70a3e2bf85b3f796964dd7ee2c43a7c3
Olav Fosse <fosseolav@gmail.com>
2020-06-07 22:53:40 +0200

    Finish exercise 1.4

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -11,9 +11,7 @@ const Part = (props) => (
 
 const Content = (props) => (
   <div>
-    <Part part={props.part1} />
-    <Part part={props.part2} />
-    <Part part={props.part3} />
+    {props.parts.map(part => (<Part part={part}/>))}
   </div>
 )
 
@@ -23,24 +21,26 @@ const Total = (props) => (
 
 const App = () => {
   const course = 'Half stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'The state of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'The state of a component',
+      exercises: 14
+    }
+    ]
 
   return (
     <div>
       <Header course={course}/>
-      <Content part1={part1} part2={part2} part3={part3}/>
-      <Total count={part1.exercises + part2.exercises + part3.exercises}/>
+      <Content parts={parts}/>
+      <Total count={parts.reduce((sum, part) => sum + part.exercises, parts[0].exercises)}/>
     </div>
   )
 }
