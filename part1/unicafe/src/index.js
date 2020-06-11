a40e00ec447edd7ebfa105a0e89f9b742efaa635
Olav Fosse <fosseolav@gmail.com>
2020-06-11 21:10:03 +0200

    Finish exercise 1.9

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -5,11 +5,14 @@ const Statistics = (props) => {
   return (
     <div>
       <h1>statistics</h1>
-      <p>good {props.statistics.good}</p>
-      <p>neutral {props.statistics.neutral}</p>
-      <p>bad {props.statistics.bad}</p>
-      <p>average {props.statistics.average}</p>
-      <p>positive {props.statistics.positive} %</p>
+      {props.statistics.good + props.statistics.bad + props.statistics.neutral ? <>
+                                <p>good {props.statistics.good}</p>
+                                <p>neutral {props.statistics.neutral}</p>
+                                <p>bad {props.statistics.bad}</p>
+                                <p>average {props.statistics.average}</p>
+                                <p>positive {props.statistics.positive} %</p>
+                              </>
+                            : <p>No feeback given</p>}
     </div>
   )
 }
@@ -19,8 +22,8 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
   const total = (good + bad + neutral)
-  const average = (good - bad) / total || 0
-  const positive = (good) / total * 100 || 0
+  const average = (good - bad) / total
+  const positive = (good) / total * 100
 
   return (
     <div>
