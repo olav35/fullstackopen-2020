8bcbce705480474f929715c054321151fd3ba898
Olav Fosse <fosseolav@gmail.com>
2020-07-09 19:29:41 +0200

    Reimplement person updating

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -19,14 +19,26 @@ const App = () => {
   const handleAddNewPerson = (event) => {
     event.preventDefault()
 
-    personService.create({name: newName, number: newNumber})
-                  .then(newPerson => {
-                    setPersons([...persons].concat(newPerson))
-                    const message = `Added ${newPerson.name}`
-                    setNotification({message, type: 'success'})
-                  }).catch(error => {
-                      setNotification({message: error.response.data.error, type: 'failure'})
-                    })
+    const errorMessage = `Information of ${newName} has already been removed from the server.`
+    let person = persons.find((p) => p.name === newName)
+    if(!person)
+      personService.create({name: newName, number: newNumber})
+                   .then(newPerson => {
+                     setPersons([...persons].concat(newPerson))
+                     const message = `Added ${newPerson.name}`
+                     setNotification({message, type: 'success'})
+                   }).catch(error => {
+                     setNotification({message: error.response.data.error, type: 'failure'})
+                   })
+    else if(window.confirm(`${newName} is already added to the phonebook, replace the old number with a new one?`)){
+      personService.update(person.id, {...person, number: newNumber}).then(newPerson => {
+        setPersons(persons.map(innerPerson =>
+          innerPerson.id === person.id ? newPerson : innerPerson
+        ))
+        const message = `Updated ${newPerson.name}`
+        setNotification({message, type: 'success'})
+      }).catch(_ => setNotification({message: errorMessage, type: 'failure'}))
+    }
     setTimeout(() => setNotification(null), 5000)
     setNewName('')
     setNewNumber('')
